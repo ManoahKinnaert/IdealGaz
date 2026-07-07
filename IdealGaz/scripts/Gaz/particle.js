52a1b576a726ec44fb809e89d88d285cdd7fb29a
Manoah Kinnaert <manoah.k@icloud.com>
2026-07-07 12:23:35 +0200

    Some changes and additions

--- IdealGaz/scripts/Gaz/particle.js
+++ IdealGaz/scripts/Gaz/particle.js
@@ -1,35 +1,38 @@
 function generateRandom(min, max) {
-    return Math.random() * (max - min) + min;
+    return Math.random() * (max - min) + min
 }
+
 class Particle {
-    constructor (size, position, velocity, pressure) {
-        this.size = size // some kind of radius
-        this.position = position // point
-        this.velocity = velocity // vector
-        this.pressure = presure // presure placeholder
+    constructor(size, position, pressure, temperature) {
+        this.size = size
+        this.position = position
+        this.temperature = temperature
+        this.pressure = pressure
+        this.color = this.computeColor()
     }
 
     render(canvas) {
         const ctx = canvas.getContext("2d")
 
-        ctx.fillStyle = this.computeColor()
+        ctx.fillStyle = this.color
         ctx.beginPath()
-        ctx.arg(this.position.x, this.position.y, 0, Math.PI * 2)
+        ctx.arc(this.position.x, this.position.y, this.size, 0, Math.PI * 2)
         ctx.fill()
     }
 
-    update(canvas) {
+    update() {
     }
-    
+
     computeColor() {
-        rLow, rHigh = 90 * this.pressure / 1000, 60 * this.pressure / 1000
-        gLow, gHigh = 155 * this.pressure / 1000, 240 * this.pressure / 1000
-        b = 250 * this.pressure / 1000 
-        r = generateRandom(rLow, rHigh)
-        g = generateRandom(gLow, gHigh)
-        return `rgb(${r}, ${g}, ${b})`
+        const p = this.pressure / 1000
+        const t = Math.min(Math.max((this.temperature - 200) / 200, 0), 1)
+
+        const r = Math.round((generateRandom(60, 90) + 80 * t) * p)
+        const g = Math.round((generateRandom(155, 240) + 20 * (1 - Math.abs(t - 0.5) * 2)) * p)
+        const b = Math.round((250 - 100 * t) * p)
+
+        return `rgb(${Math.min(r, 255)}, ${Math.min(g, 255)}, ${Math.min(b, 255)})`
     }
 }
-
 
 export { Particle }
--- IdealGaz/scripts/index.js
+++ IdealGaz/scripts/index.js
@@ -2,15 +2,25 @@ import { Particle } from './Gaz/particle.js'
 import { Point } from './Gaz/math/point.js'
 import { Vector } from './Gaz/math/vector.js'
 
+
+let PRESSURE = 1000
+let T = 273
+
 const canvas = document.getElementById("gaz-container")
 canvas.width = 400
 canvas.height = 400
 var ctx = canvas.getContext("2d");
 
 let particles = []
-let running = false 
+let running = true 
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
 function spawnNew(radius=10) {
+    let particle = new Particle(radius, new Point(randomInt(20, canvas.width - 20), randomInt(20, canvas.height - 20)), PRESSURE, T)
+    particles.push(particle)
 }
 
 function spawnN(n=5) {
@@ -25,4 +35,13 @@ function render(val, index, array) {
 }
 
 function runloop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    particles.forEach(update)
+    particles.forEach(render)
+
+    if (running) requestAnimationFrame(runloop)
 }
+
+spawnNew()
+runloop()
